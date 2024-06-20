ee1630fde2a14e26c563fb93cc0f86da0f72cce1
nam127 <buithanhnam9109@gmail.com>
2024-06-20 14:17:56 +0700

    get all slots of a yard by yardID

--- src/main/java/com/main/badminton/booking/repository/SlotRepository.java
+++ src/main/java/com/main/badminton/booking/repository/SlotRepository.java
@@ -1,9 +1,16 @@
 package com.main.badminton.booking.repository;
 
 import com.main.badminton.booking.entity.Slots;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface SlotRepository extends JpaRepository<Slots, Long> {
+public interface SlotRepository extends JpaRepository<Slots, Integer> {
+    @Query(value = "SELECT u FROM Slots u WHERE u.yards.id =:yardId")
+    List<Slots> findByYardId(@Param("yardId") Integer yardId, Pageable pageable);
 }
