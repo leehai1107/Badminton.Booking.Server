1b341319a5e9e2efcc2420fd793be973c56b1cca
leehai1107 <leehai1107@gmail.com>
2024-06-24 14:06:45 +0700

    update :fix model mapper for booking orders

--- src/main/java/com/main/badminton/booking/converter/BookingOrdersConverter.java
+++ src/main/java/com/main/badminton/booking/converter/BookingOrdersConverter.java
@@ -2,7 +2,7 @@ package com.main.badminton.booking.converter;
 
 import com.main.badminton.booking.dto.request.BookingOrdersRequestDTO;
 import com.main.badminton.booking.dto.response.BookingOrdersResponseDTO;
-import com.main.badminton.booking.entity.BookingOrders;
+import com.main.badminton.booking.entity.*;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -17,11 +17,41 @@ public class BookingOrdersConverter {
         this.modelMapper = modelMapper;
     }
 
-    public BookingOrders requestDtoToEntity(BookingOrdersRequestDTO requestDTO) {
-        return modelMapper.map(requestDTO, BookingOrders.class);
-    }
+// Convert BookingOrdersRequestDTO to BookingOrders
+public BookingOrders requestDtoToEntity(BookingOrdersRequestDTO bookingOrdersRequestDTO) {
+    BookingOrders bookingOrders = modelMapper.map(bookingOrdersRequestDTO, BookingOrders.class);
 
-    public BookingOrdersResponseDTO entityToResponseDto(BookingOrders entity) {
-        return modelMapper.map(entity, BookingOrdersResponseDTO.class);
+    // Manually set the references to the other entities
+    Yards yards = new Yards();
+    yards.setId(bookingOrdersRequestDTO.getYardId());
+    bookingOrders.setYards(yards);
+
+    User user = new User();
+    user.setId(bookingOrdersRequestDTO.getUserId());
+    bookingOrders.setUser(user);
+
+    Schedules schedules = new Schedules();
+    schedules.setId(bookingOrdersRequestDTO.getScheduleId());
+    bookingOrders.setSchedules(schedules);
+
+    Slots slots = new Slots();
+    slots.setId(bookingOrdersRequestDTO.getSlotId());
+    bookingOrders.setSlots(slots);
+
+    return bookingOrders;
+}
+
+
+    // Convert BookingOrders to BookingOrdersResponseDTO
+    public BookingOrdersResponseDTO entityToResponseDto(BookingOrders bookingOrders) {
+        BookingOrdersResponseDTO dto = new BookingOrdersResponseDTO();
+
+        dto.setYardId(bookingOrders.getYards().getId());
+        dto.setUserId(bookingOrders.getUser().getId());
+        dto.setScheduleId(bookingOrders.getSchedules().getId());
+        dto.setSlotId(bookingOrders.getSlots().getId());
+        dto.setStatus(bookingOrders.getStatus());
+
+        return dto;
     }
 }
