833c2b40ebd6a0714b45dd9112e7cfc9e31d1fb8
leehai1107 <leehai1107@gmail.com>
2024-07-13 18:58:45 +0700

    update: fix native query

--- src/main/java/com/main/badminton/booking/repository/BookingOrdersRepository.java
+++ src/main/java/com/main/badminton/booking/repository/BookingOrdersRepository.java
@@ -13,7 +13,7 @@ import java.util.List;
 public interface BookingOrdersRepository extends JpaRepository<BookingOrders, Integer> {
     List<BookingOrders> findByUserId(Integer userId);
 
-    @Query(value = "SELECT * FROM BookingOrders bo WHERE bo.bookingAt < (:currentTime - INTERVAL 5 MINUTE) AND bo.status IS NULL", nativeQuery = true)
+    @Query(value = "SELECT bo FROM BookingOrders bo WHERE bo.bookingAt < (:currentTime - INTERVAL 5 MINUTE) AND bo.status IS NULL")
     List<BookingOrders> findExpiredBooking(LocalDateTime currentTime);
 
     @Query("SELECT COUNT(*) FROM BookingOrders bo WHERE bo.yards.id = :yardId AND bo.slots.id = :slotId AND bo.tournamentEnd >= :tournamentStart AND (bo.status IS NULL OR bo.status IS true)")
